b66496400affa72c39a708e23947222c76b8f5d3
brnunn00 <brnunn00@gmail.com>
2019-06-01 19:55:24 -0700

    finito

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+var connection;
+if (process.env.JAWSDB_URL){
+  mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+connection =  mysql.createConnection({
+  host: 'localhost',
+  user: 'root', 
+  password: 'root', 
+  database: 'burgerss_db'
+})
+}
 
 connection.connect(function(err) {
   if (err) {
